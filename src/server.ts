f9999f3d0c0ea4012ba50761414a5f77829e44bb
Lars Kinder <lars.kinder@porsche.de>
2020-12-11 14:25:07 +0100

    Added 200 status on succesful get request

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ import {filterImageFromURL, deleteLocalFiles, isUrl, getLocalFiles} from './util
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get("/filteredimage",async (req, res) => {
+  app.get("/filteredimage",async (req:express.Request, res:express.Response) => {
     const image_url = req.query.image_url;
 
     if(!image_url || image_url==0){
@@ -27,33 +27,15 @@ import {filterImageFromURL, deleteLocalFiles, isUrl, getLocalFiles} from './util
 
     const filtered_path = await filterImageFromURL(image_url);
 
-    return res.sendFile(filtered_path, () => {
+    return res.status(200).sendFile(filtered_path, () => {
       deleteLocalFiles(getLocalFiles());
       console.info("File deleted.")
     });
   });
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
+  app.get( "/", async ( req:express.Request, res:express.Response ) => {
     res.send("try GET /filteredimage?image_url={{}}")
   } );
   
